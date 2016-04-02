b203afd38109f1a5ee031c2a8e83d93d92610f90
mockingbird <Stanislavrussu92@gmail.com>
2016-04-02 14:21:44 +0300

    Добавил проект JobTalks

---
+++ scroll_pop-up.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function(){
+
+    PopUpHide(); //Скрыть PopUp при загрузке страницы
+
+	var $menu = $("#menu");
+        $(window).scroll(function(){
+            if ( $(this).scrollTop() > 900 && $menu.hasClass("default") ){
+                $menu.removeClass("default").addClass("fixed");
+            } else if($(this).scrollTop() <= 900 && $menu.hasClass("fixed")) {
+                $menu.removeClass("fixed").addClass("default");
+            }
+        }); //scroll
+      
+    });
+
+    //Функция отображения PopUp
+    function PopUpShow(){
+        $("#popup").show();
+    }
+    //Функция скрытия PopUp
+    function PopUpHide(){
+        $("#popup").hide();
+    }
